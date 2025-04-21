dfb0dd19dd92c9668d771bbee081389ecb8fc66c
Lê Minh Tiến <leminhtien12c1thdao@gmail.com>
2025-04-21 09:46:59 +0700

    Cập nhập main

--- Tables/dbo.Categories.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Categories] (
-  [CategoryID] [int] IDENTITY,
-  [CategoryName] [nvarchar](100) NOT NULL,
-  [CreatedDate] [datetime] NOT NULL DEFAULT (getdate()),
-  PRIMARY KEY CLUSTERED ([CategoryID]),
-  UNIQUE ([CategoryName])
-)
-ON [PRIMARY]
-GO

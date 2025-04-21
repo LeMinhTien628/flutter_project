be13a08068b7ddd4e1cf46fc81c30406a3602df5
Lê Minh Tiến <leminhtien12c1thdao@gmail.com>
2025-04-21 10:30:35 +0700

    Cập nhập lại bảng Promotion

--- Tables/dbo.Promotions.sql
+++ Tables/dbo.Promotions.sql
@@ -4,6 +4,7 @@
   [Description] [nvarchar](255) NULL,
   [DiscountPercentage] [decimal](5, 2) NULL,
   [ImageURL] [nvarchar](255) NULL,
+  [IsMainPromotion] [int] NULL,
   [StartDate] [datetime] NOT NULL,
   [EndDate] [datetime] NOT NULL,
   [CreatedDate] [datetime] NOT NULL DEFAULT (getdate()),
